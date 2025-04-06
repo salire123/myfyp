79b42e8629e0a65ababf467564977df883d59652
salire123 <simonli123hk@gmail.com>
2025-04-06 20:45:55 +0800

    refactor: Clean up main.ipynb by removing redundant commands and adding an empty code cell; update utils __init__.py to streamline imports

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,5 @@
-from .traing_dataset import convert_dataset_and_save_as_file, load_jsonl, tokenize_the_data
+from .traing_dataset import convert_dataset_and_save_as_file
+from .train import train
 
 __all__ = [
     "convert_dataset_and_save_as_file",
